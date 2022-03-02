f2f3db3894b4fd9cbe47dfe519c5cfa66caa4f32
juantoman <juan.torres@iestacio.com>
2022-03-02 16:36:20 +0100

    Updates

---
+++ precache-manifest.2282033af27759374861997f835080cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ed178b83f3966b4f01c532fbd8e3be",
+    "url": "/tutorial-react/index.html"
+  },
+  {
+    "revision": "4e0ceda58e57f760d605",
+    "url": "/tutorial-react/static/css/main.794000d0.chunk.css"
+  },
+  {
+    "revision": "ffcf453ea9eab41f947d",
+    "url": "/tutorial-react/static/js/2.230b6b81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutorial-react/static/js/2.230b6b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0ceda58e57f760d605",
+    "url": "/tutorial-react/static/js/main.e1714b8a.chunk.js"
+  },
+  {
+    "revision": "682edddca3f6012c4544",
+    "url": "/tutorial-react/static/js/runtime-main.4b9bd8d2.js"
+  }
+]);
